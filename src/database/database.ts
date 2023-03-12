81234251862a72dd1e44d5c13c103269fda4866a
Thomas Wieck <thomas.wieck@hotmail.co.uk>
2023-03-12 11:04:45 +0000

    Set up real database instead of using in memory using .env files

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,15 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
 
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+const connString = "postgres://bookshopuser:super-secret-password@127.0.0.1:5432/bookshop";
+// TODO: Replace the above string with a string built from YOUR process.env.DB_USERNAME etc variables
+
+export let sequelize = new Sequelize(connString);
+if (process.env.NODE_ENV !== 'test') {
+    sequelize = new Sequelize(
+        process.env.DB_NAME ?? 'MISSING_DB_NAME_CONFIG',
+        process.env.DB_USERNAME ?? 'MISSING_DB_USERNAME_CONFIG',
+        process.env.DB_PASSWORD ?? 'MISSING_DB_PASSWORD_CONFIG', {
+        host: process.env.DB_HOST ?? 'MISSING_DB_HOST_CONFIG',
+        dialect: (process.env.DB_DIALECT as Dialect) ?? 'postgres',
+    });
+}
